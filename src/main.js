9f9e8d78014bbb4bdfad6767bea839f94bb1aeb9
Andrii Lesnicov <andriilesnicov@gmail.com>
2025-05-12 12:40:42 +0200

    add refactoring to HW-11

---
+++ src/main.js
@@ -0,0 +1,62 @@
+import { getImagesByQuery } from './js/pixabay-api.js';
+import {
+  createGallery,
+  clearGallery,
+  showLoader,
+  hideLoader,
+} from './js/render-functions.js';
+
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+import './css/loader.css';
+
+const formEl = document.querySelector('.form');
+const inputEl = formEl.querySelector('input[name="search-text"]');
+
+formEl.addEventListener('submit', handleFormSubmit);
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+  event.target.disabled = true;
+  const query = inputEl.value.trim();
+  iziToast.destroy();
+
+  if (query === '') {
+    iziToast.warning({
+      message: 'Please enter a search query.',
+      position: 'topRight',
+    });
+    return;
+  }
+
+  showLoader();
+  clearGallery();
+
+  fetchImages(query)
+    .then(images => {
+      if (images.length === 0) {
+        iziToast.info({
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+          position: 'topRight',
+        });
+        return;
+      }
+      createGallery(images);
+    })
+    .catch(error => {
+      iziToast.error({
+        message: 'Something went wrong. Please try again later.',
+        position: 'topRight',
+      });
+      console.error(error);
+    })
+    .finally(() => {
+      hideLoader();
+      event.target.disabled = false;
+    });
+}
+
+function fetchImages(query) {
+  return getImagesByQuery(query).then(data => data.hits);
+}
